834808d94da0b58003f015912885eb0852883046
Md Al Amin <alamin.hawalder69@gmail.com>
2017-12-12 05:10:54 -0500

    add method no 1

--- src/methods/MethodPractice.java
+++ src/methods/MethodPractice.java
@@ -4,6 +4,27 @@ public class MethodPractice {
 	
 	public static void main(String[] args){
 		
+		System.out.println("Q no-1:" +getName("ma a amin"));
+		
+		
+		
+		
+	}
+	
+	/**
+	 * Q no:1
+	 * write a method that take your name input and return your name
+	 * parameter:String name
+	 * returnType:String text
+	 * ServingBucket:variable
+	 */
+	public static String getName(String name){
+		String text = null;
+		
+		text = name;
+		
+	
+		return text;
 	}
 
 }
